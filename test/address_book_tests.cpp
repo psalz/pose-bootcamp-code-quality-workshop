658cda9760a27678d87530d12f2ed7df950a77df
Julian Muenzberg <muenzberg.julian@gmail.com>
2024-05-08 09:23:29 +0000

    Add new test which checks the string length when adding a new entry.

--- test/address_book_tests.cpp
+++ test/address_book_tests.cpp
@@ -1,7 +1,10 @@
 #include <catch2/catch_test_macros.hpp>
 #include <catch2/matchers/catch_matchers_string.hpp>
+#include <string>
 
 #include "address_book.hpp"
+
+using namespace std::string_literals;
 
 TEST_CASE("entries can be added and removed") {
 	address_book ab;
@@ -10,4 +13,23 @@ TEST_CASE("entries can be added and removed") {
 	CHECK(ab.has_entry("Jane Doe"));
 	ab.remove_entry("Jane Doe");
 	CHECK_FALSE(ab.has_entry("Jane Doe"));
+}
+
+TEST_CASE("names must be between 1 and 100 characters long") {
+	address_book ab;
+	std::string str_empty = "";
+	CHECK(str_empty.length() == 0);
+	CHECK_THROWS(ab.add_entry(str_empty));
+	std::string str_101 = "AAAAAaaaaa"
+						  "AAAAAaaaaa"
+						  "AAAAAaaaaa"
+						  "AAAAAaaaaa"
+						  "AAAAAaaaaa"
+						  "AAAAAaaaaa"
+						  "AAAAAaaaaa"
+						  "AAAAAaaaaa"
+						  "AAAAAaaaaa"
+						  "AAAAAaaaaaB";
+	CHECK(str_101.length() == 101);
+	CHECK_THROWS(ab.add_entry(str_101));
 }
