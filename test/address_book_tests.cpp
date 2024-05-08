0905000d74691dd5001ef6b7ec319b4c9c7b77d4
bernhard <a.b@c.d>
2024-05-08 12:07:08 +0000

    add synchronization tests

--- test/address_book_tests.cpp
+++ test/address_book_tests.cpp
@@ -14,8 +14,47 @@ TEST_CASE("entries can be added and removed") {
 
 TEST_CASE("entries can be added") {
 	address_book ab;
+    ab.add_entry("huuu");
 	ab.add_entry("buh");
+    ab.add_entry("waaah");
 	CHECK(ab.has_entry("buh"));
 	CHECK(ab.has_entry("BUH"));
 	CHECK_THROWS(ab.add_entry(""));
-	CHECK(ab.get_entries());}
+	CAPTURE(ab.get_entries());
+	CHECK("Buh" == ab.get_entries()[0]);
+//	FAIL();
+}
+
+class variable_synchronization_provider : public synchronization_provider {
+  public:
+	variable_synchronization_provider(std::string path);
+	std::vector<std::string> synchronize(std::vector<std::string> serialized_entries) override;
+
+  private:
+	std::string m_path;
+};
+
+variable_synchronization_provider::variable_synchronization_provider(std::string path) : m_path(path) {}
+
+std::vector<std::string> variable_synchronization_provider::synchronize(std::vector<std::string> serialized_entries) {
+	std::vector<std::string> merged_entries = serialized_entries;
+
+	return merged_entries;
+}
+
+TEST_CASE("sychronized database tests") {
+	address_book ab;
+	variable_synchronization_provider provider{""};
+	ab.synchronize(provider);
+	CHECK_FALSE(ab.has_entry("buh"));
+
+	ab.add_entry("buh");
+	CHECK(ab.has_entry("buh"));
+	ab.synchronize(provider);
+	CHECK(ab.has_entry("buh"));
+	ab.remove_entry("buh");
+	CHECK_FALSE(ab.has_entry("buh"));
+	ab.synchronize(provider);
+	//CHECK(ab.has_entry("buh"));
+
+}
